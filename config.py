dc798026f3ae7a9665ba4e04b18ff8eb63288c33
unknown <administrator@XCSOLUCOES.LOCAL>
2022-12-11 00:22:05 -0300

    con_bd

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ SECRET_KEY = 'master'
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
-        usuario = 'root',
-        senha = 'root',
+        usuario = 'master',
+        senha = 'master123',
         servidor = '127.0.0.1',
         database = 'app_admin'
     )
